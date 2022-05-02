2d3a798368bc1550d5bdc642e6e31cc46b577d62
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-05-02 15:16:06 +0200

    init

---
+++ altriProdotti.php
@@ -0,0 +1,62 @@
+<?php
+include_once 'prodotto.php';
+
+class Alimento extends Prodotto {
+    private $peso;
+    private $tipo;
+    
+    function __construct(string $_nome, float $_prezzo, string $_img, float $_peso, string $_tipo){
+        parent::__construct($_nome, $_prezzo, $_img);
+        $this->peso = $_peso;
+        $this->tipo = $_tipo;
+    }
+
+    public function setPeso($_peso) {
+        $this->peso = $_peso;
+    }
+
+    public function getPeso() {
+        return $this->peso;
+    }
+
+    public function setTipo($_tipo) {
+        $this->tipo = $_tipo;
+    }
+
+    public function getTipo() {
+        return $this->tipo;
+    }
+}
+
+class Antipulci extends Prodotto {
+    private $razzaAnimale;
+    private $disponibilità;
+    
+    function __construct(string $_nome, float $_prezzo, string $_img ,$_razzaAnimale , $_disponibilità){
+        parent::__construct($_nome, $_prezzo, $_img);
+        $this->razzaAnimale = $_razzaAnimale;
+        $this->disponibilità = $_disponibilità;
+    }
+
+    public function setRazzaAnimale($_razzaAnimale) {
+        $this->razzaAnimale = $_razzaAnimale;
+    }
+
+    public function getRazzaAnimale() {
+        return $this->razzaAnimale;
+    }
+
+    public function setDisponibilità($_disponibilità) {
+        $this->disponibilità = $_disponibilità;
+    }
+
+    public function getDisponibilità() {
+        return $this->disponibilità;
+    }
+}
+
+$zeroPulci= new Antipulci("Zero Pulci", 15.30, "immagine Antipulci","Labrador","Mese: aprile");
+var_dump($zeroPulci);
+
+$croccantini= new Alimento ("pappa", 12.60, "immagine pappa", 3.50, "croccatini anallergici");
+var_dump($croccantini);
---
+++ prodotto.php
@@ -0,0 +1,54 @@
+<?php
+
+class Prodotto
+{
+    private string $prodottoNome;
+    private float $prodottoPrezzo;
+    private int $prodottoSconto = 0;
+    private string $prodottoImg;
+
+    function __construct(string $_nome, float $_prezzo, string $_img) {
+        $this->prodottoNome = $_nome;
+        $this->prodottoPrezzo = $_prezzo;
+        $this->prodottoSconto = 0;
+        $this->prodottoImg = $_img;
+    }
+    
+    public function setProdottoNome($_nome) {
+        $this->prodottoNome = $_nome;
+    }
+
+    public function getProdottoNome() {
+        return $this->prodottoNome;
+    }
+
+    public function setProdottoPrezzo($_prezzo) {
+        $this->prodottoPrezzo = $_prezzo;
+    }
+
+    public function getProdottoPrezzo() {
+        return $this->prodottoPrezzo;
+    }
+
+    public function setProdottoSconto($_sconto) {
+        $this->prodottoSconto = $_sconto;
+    }
+
+    public function getProdottoSconto() {
+        return $this->prodottoSconto;
+    }
+
+    public function setProdottoImg($_img) {
+        $this->prodottoImg = $_img;
+    }
+
+    public function getProdottoImg() {
+        return $this->prodottoImg;
+    }
+}
+
+$cuccia = new Prodotto("cuccia", 12.40, "immagine_cuccia");
+var_dump($cuccia);
+
+$peluche= new Prodotto("peluche", 15.00, "immagine_peluche");
+var_dump($peluche);
