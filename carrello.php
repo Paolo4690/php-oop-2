645cdfbeeec410b039c244701fc4981d455428a2
Paolo4690 <paolo.calafiore.90@gmail.com>
2022-05-02 18:21:38 +0200

    fuinzione sconto da sistemare

--- carrello.php
+++ carrello.php
@@ -2,12 +2,23 @@
 include_once __DIR__ . '/prodotti.php';
 
 class Carrello{
-    public $prodotti = [];
-    public $totale;
+    private $prodotti = [];
+    public $totale; //da CAMBIARE
 
     public function aggiungiProdotto($_nome, $_prezzo, $_img){
         array_push($this->prodotti, new ProdottoGenerale($_nome, $_prezzo, $_img));
         $this->totale += $_prezzo;
+    }
+    public function setTotale($_prezzo){
+        $this->totale += $_prezzo;
+    }
+
+    public function getTotale(){
+        return $this->totale;
+    }
+
+    public function getProdotto(){
+        return $this->prodotti;
     }
 }
 
@@ -17,4 +28,4 @@ $carrello->aggiungiProdotto('cuccia', 12.40, 'immagine_cuccia');
 $carrello->aggiungiProdotto('Zero Pulci', 15.30, 'immagine Antipulci');
 $carrello->aggiungiProdotto('pappa', 12.60, 'immagine pappa');
 
-var_dump($carrello);
+// var_dump($carrello);
--- utente.php
+++ utente.php
@@ -1,5 +1,6 @@
 <?php
 include_once __DIR__ . '/cartaCredito.php';
+include_once __DIR__ . '/carrello.php';
 
 class Utente
 {
@@ -8,12 +9,14 @@ class Utente
     private $utenteCognome;
     private $utenteIndirizzo;
     private $utenteMetodoPagamento= []; 
+    protected $carrello;
 
     public function __construct($_utenteId, $_utenteNome, $_utenteCognome, $_utenteIndirizzo){
         $this->utenteId = $_utenteId;
         $this->utenteNome = $_utenteNome;
         $this->utenteCognome = $_utenteCognome;
         $this->utenteIndirizzo = $_utenteIndirizzo;
+        $this->carrello = new Carrello;
     }
 
     public function setUtenteId($_utenteId) {
@@ -56,11 +59,17 @@ class Utente
         array_push($this->utenteMetodoPagamento, new Carta_Credito($_numero, $_banca, $_scadenza, $_proprietario, $_cvc));
     }
 
+    public function aggiungiAlCarrello($_nome, $_prezzo, $_img){
+        $obj = $this->carrello;
+        $obj->aggiungiProdotto($_nome, $_prezzo, $_img);
+    }
 
 }
 
-$utente1 = new Utente(1, "Mario", "Rossi", "Via Roma, 1");
-$utente1->aggiungiCarta("1234567891234567","banca","10 September 2024","signor signore",456);
+$utente1 = new Utente(1, 'Mario', 'Rossi', 'Via Roma, 1');
+$utente1->aggiungiCarta('1234567891234567', 'banca', '10 September 2024', 'signor signore', 456);
+$utente1->aggiungiAlCarrello('oggetto aggiunto', 300, 'immagine');
+$utente1->aggiungiAlCarrello('Altro oggetto aggiunto', 600,'altra immagine');
 var_dump($utente1);
 
 class Utente_registrato extends Utente
@@ -68,6 +77,7 @@ class Utente_registrato extends Utente
     private $mail;
     private $usarname;
     private $password;
+    private $sconto = 20;
 
     public function __construct($_utenteId, $_utenteNome, $_utenteCognome, $_utenteIndirizzo, $_mail, $_usarname, $_password){
         parent::__construct($_utenteId, $_utenteNome, $_utenteCognome, $_utenteIndirizzo);
@@ -102,9 +112,21 @@ class Utente_registrato extends Utente
     public function getPassword() {
         return $this->password;
     }
+    
+    public function getSconto() {
+        return $this->sconto;
+    }
+
+    public function aggiungiAlCarrello($_nome, $_prezzo, $_img){
+        $obj = $this->carrello;
+        $obj->aggiungiProdotto($_nome, $_prezzo, $_img);
+        $obj->totale = $obj->totale - ($obj->totale * $this->sconto / 100);
+    }
 
 }
 
 $utente2 = new Utente_registrato(2, "Mario", "Rossi", "Via Roma, 1", "la mail", "l'username", "la password");
 $utente2->aggiungiCarta("1234567891234567","un altra banca","20 September 2024","Mario Rossi",456);
+$utente2->aggiungiAlCarrello('oggetto aggiunto', 300, 'immagine');
+$utente2->aggiungiAlCarrello('Altro oggetto aggiunto', 600,'altra immagine');
 var_dump($utente2);
